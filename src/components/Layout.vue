55ff723491d0a282830fe4470f7802bc10d69f7b
souldough <yipingh@yeah.net>
2018-03-05 15:18:37 +0800

    制作首页

---
+++ src/components/Layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <v-header></v-header>
+    <v-main></v-main>
+    <v-footer></v-footer>
+  </div>
+</template>
+<script>
+import VHeader from '@/components/header/Header'
+import VFooter from '@/components/footer/footer'
+import VMain from '@/components/main/Main'
+  export default {
+    name: 'layout', 
+    components: {
+      VHeader,
+      VFooter,
+      VMain
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+.page
+  width 1200px
+  margin 0 auto
+</style>
+
+
---
+++ src/components/entryMenu/entryMenu.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <div class="menu">
+    <a href=""><img src='./registerLogin.jpg'></a>
+    <a href=""><img src='./reserve.jpg'></a>
+    <a href=""><img src='./setting.jpg'></a>
+    <a href=""><img src='./policy.jpg'></a>
+  </div>
+</div>  
+</template>
+<script>
+  export default {
+    name: 'entryMenu'
+  }
+</script>
+<style lang="stylus" scoped>
+.menu
+  display flex
+  justify-content space-between
+  margin-top 32px
+</style>
+
